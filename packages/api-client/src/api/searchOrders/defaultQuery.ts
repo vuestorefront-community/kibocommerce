504bf5741aa430538d6b6ec86790398479e828c2
Cole McMannus <colemcmannus@gmail.coms>
2021-07-16 15:16:18 -0400

    order history; final; 3

--- packages/api-client/src/api/searchOrders/defaultQuery.ts
+++ packages/api-client/src/api/searchOrders/defaultQuery.ts
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export default gql`
-query {
-  orders {
+query ($filter: String, $startIndex: Int, $pageSize: Int){
+  orders(filter: $filter, startIndex: $startIndex, pageSize: $pageSize) {
     pageCount
     items {
       auditInfo {
