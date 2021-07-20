8b6534ca7668603a8b156d6532cf301ce91395cb
Chandradeepta <43542673+Chandradeepta@users.noreply.github.com>
2021-07-20 11:19:17 +0530

    Customer Data Update params type added

--- packages/api-client/src/api/updateCustomerPersonalData/index.ts
+++ packages/api-client/src/api/updateCustomerPersonalData/index.ts
@@ -1,26 +1,12 @@
-import { CustomerAccount } from './../../types/GraphQL';
+import { IUpdateCustomerDataParams } from './../../types/Api';
 import { Context } from '@vue-storefront/core';
+import { CustomerResponse } from '../../types/Api';
 import updateCustomerData from './defaultMutation';
 
 const updateCustomerPersonalData = async (
   context: Context,
-  params: {
-    accountId: number;
-    customerAccountInput: {
-      emailAddress: string;
-      userName: string;
-      firstName: string;
-      lastName: string;
-      isAnonymous: boolean;
-      isLocked: boolean;
-      isActive: boolean;
-      acceptsMarketing: boolean;
-      hasExternalPassword: boolean;
-      id: number;
-      taxExempt: boolean;
-    };
-  }
-): Promise<CustomerAccount> => {
+  params: IUpdateCustomerDataParams
+): Promise<CustomerResponse> => {
   return await context.client.mutate({
     mutation: updateCustomerData,
     variables: params,
--- packages/api-client/src/types/Api.ts
+++ packages/api-client/src/types/Api.ts
@@ -2,17 +2,35 @@
 import { ApolloQueryResult } from 'apollo-client';
 import { FetchResult } from 'apollo-link';
 
-import {
-    CategoryCollection,
-    Order,
-    CustomerAccount
-} from './GraphQL'
+import { CategoryCollection, Order, CustomerAccount } from './GraphQL';
 
-export type QueryResponse<K extends string, V> = ApolloQueryResult<Record<K, V>>;
+export type QueryResponse<K extends string, V> = ApolloQueryResult<
+  Record<K, V>
+>;
 export type OrderQueryResponse = QueryResponse<'order', Order>;
 export type OrderMutationResponse = QueryResponse<'order', Order>;
 
-export type CustomerResponse = QueryResponse<'customerAccount', CustomerAccount>
+export type CustomerResponse = QueryResponse<
+  'customerAccount',
+  CustomerAccount
+>;
 interface ApiMethods {
-    getCategory (params): Promise<QueryResponse<'categories', CategoryCollection>>;
+  getCategory(params): Promise<QueryResponse<'categories', CategoryCollection>>;
+}
+
+export interface IUpdateCustomerDataParams {
+  accountId: number;
+  customerAccountInput: {
+    emailAddress: string;
+    userName: string;
+    firstName: string;
+    lastName: string;
+    isAnonymous: boolean;
+    isLocked: boolean;
+    isActive: boolean;
+    acceptsMarketing: boolean;
+    hasExternalPassword: boolean;
+    id: number;
+    taxExempt: boolean;
+  };
 }
