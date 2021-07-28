556052723dd162be2d81b32dce11cedcac2b86a9
SushantJadhav <Sushant.Jadhav@kibocommerce.com>
2021-07-28 15:23:09 +0530

    Git billing info changes

--- packages/api-client/src/api/getBillingInfo/index.ts
+++ packages/api-client/src/api/getBillingInfo/index.ts
@@ -1,8 +1,8 @@
 import { Context, CustomQuery} from '@vue-storefront/core';
 import defaultQuery from './defaultQuery';
-// import { GetBillingInfoParams, GetBillingInfoResponse } from '../../types/Api';
+import { GetBillingInfoParams, GetBillingInfoResponse } from '../../types/Api';
 
-export default async (context: Context, params: {orderId: string}, customQuery?: CustomQuery) => {
+export default async (context: Context, params: GetBillingInfoParams, customQuery?: CustomQuery): Promise<GetBillingInfoResponse> => {
 
   const { billingInfo } = context.extendQuery(customQuery,
     { billingInfo: { query: defaultQuery, variables: params } }
