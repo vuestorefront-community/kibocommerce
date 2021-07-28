563c45280720aefad884b6a8386f7ed3b28c7734
Kevin Watts <kevin.watts@systems-MacBook-Pro.local>
2021-07-27 21:19:08 -0500

    update config files to point at dev10

--- packages/api-client/src/mozuConfig.ts
+++ packages/api-client/src/mozuConfig.ts
@@ -1,8 +1,8 @@
 export default {
   api: {
-    accessTokenUrl: process.env.KIBO_ACCESS_TOKEN_URL || 'https://home.dev07.kubedev.kibo-dev.com/api/platform/applications/authtickets/oauth',
+    accessTokenUrl: process.env.KIBO_ACCESS_TOKEN_URL || 'https://home.dev10.kubedev.kibo-dev.com/api/platform/applications/authtickets/oauth',
     clientId: process.env.KIBO_CLIENT_ID || 'mozuadmin.mpoc_app.1.0.0.Release',
     sharedSecret: process.env.KIBO_SHARED_SECRET || '1d16d7d35b8542cba2f99216f20dca32',
-    apiHost: process.env.KIBO_API_HOST || 'https://t17194-s21127.dev07.kubedev.kibo-dev.com'
+    apiHost: process.env.KIBO_API_HOST || 'https://t17194-s21127.dev10.kubedev.kibo-dev.com'
   }
 };
