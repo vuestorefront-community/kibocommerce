08ef7628032684a35c0398b1e94380a6c64c759a
Chandradeepta <43542673+Chandradeepta@users.noreply.github.com>
2022-01-14 14:55:18 +0530

    fix(buildProductSearchVariables): accepting filter as a param

--- packages/api-client/src/api/searchProducts/_util.ts
+++ packages/api-client/src/api/searchProducts/_util.ts
@@ -9,10 +9,13 @@ function getFacetValueFilter(categoryCode, filters = []) {
 export const buildProductSearchVars = ({
   categoryCode = null,
   pageSize = 5,
+  // Filters received from URL for facetFilterList
   filters = {},
   startIndex = 0,
   sort = '',
   search = '',
+  // Filter to filter all product list e.g based on categoryCode
+  filter = ''
 }: {
   categoryCode: null;
   pageSize: number;
@@ -20,19 +23,19 @@ export const buildProductSearchVars = ({
   startIndex: number;
   sort: string;
   search: string;
+  filter: string;
 }): any => {
   let facetTemplate = '';
-  let filter = '';
   if (categoryCode) {
     facetTemplate = `categoryCode:${categoryCode}`;
-    filter = `categoryCode req ${categoryCode}`;
+    filter = filter || `categoryCode req ${categoryCode}`;
   }
   const facetFilterList = Object.keys(filters)
     .filter((k) => filters[k].length)
     .reduce((accum, k) => {
       return [
         ...accum,
-        ...filters[k].map((facetValue) => `Tenant~${k}:${facetValue}`),
+        ...filters[k].map((facetValue) => `Tenant~${k}:${facetValue}`)
       ];
     }, []);
 
@@ -42,8 +45,8 @@ export const buildProductSearchVars = ({
     startIndex,
     pageSize,
     sortBy: sort,
-    filter: filter,
+    filter,
     facetTemplate,
-    facetValueFilter,
+    facetValueFilter
   };
 };
