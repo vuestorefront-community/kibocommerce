3dceef6c85f8de162ed468934702acdc890ff3d0
Chandradeepta <chandradeepta.laha@kibocommerce.com>
2022-04-06 01:11:48 +0530

    fix: updating gql-client

--- packages/api-client/src/api/logInUser/index.ts
+++ packages/api-client/src/api/logInUser/index.ts
@@ -1,12 +1,42 @@
 /* eslint camelcase: "warn"*/
 import { Context } from '@vue-storefront/core';
-import { KiboApolloClient } from '@kibocommerce/graphql-client';
+import loginMutation from './defaultMutation';
 import { LogInUserParams, LogInUserResponse } from '../../types/Api';
+import { KiboApolloClient } from '@kibocommerce/graphql-client';
 
-const loginUser = async (context:Context, params:LogInUserParams): Promise<LogInUserResponse> => {
-  const client = context.client as KiboApolloClient;
+const loginUser = async (
+  context: Context,
+  params: LogInUserParams
+): Promise<LogInUserResponse> => {
   const { username, password } = params;
-  const loginResponse = await client.loginCustomerAndSetAuthTicket({ username, password }) as any;
+  const client = context.client as KiboApolloClient;
+  const loginResponse = await context.client.mutate({
+    mutation: loginMutation,
+    variables: {
+      loginInput: {
+        username,
+        password
+      }
+    },
+    fetchPolicy: 'no-cache'
+  });
+
+  const {
+    accessToken,
+    accessTokenExpiration,
+    refreshToken,
+    userId,
+    refreshTokenExpiration
+  } = loginResponse.data.account;
+
+  client.shopperAuthManager.setTicket({
+    accessToken,
+    accessTokenExpiration,
+    refreshToken,
+    userId,
+    refreshTokenExpiration
+  });
+
   return loginResponse;
 };
 
